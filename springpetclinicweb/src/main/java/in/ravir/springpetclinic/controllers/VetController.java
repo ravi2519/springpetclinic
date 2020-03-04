39cfb6d18301a5efca1b4b86504df126fb73a49e
Ravi Ranjan <ravi2519@gmail.com>
2020-03-04 05:50:22 -0800

    List of vets changes added closes #13

--- springpetclinicweb/src/main/java/in/ravir/springpetclinic/controllers/VetController.java
+++ springpetclinicweb/src/main/java/in/ravir/springpetclinic/controllers/VetController.java
@@ -1,7 +1,10 @@
 package in.ravir.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import in.ravir.springpetclinic.service.VetService;
 
 /**
  * VetController
@@ -9,9 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+  
+
     @RequestMapping({"/vets", "vets/index.html", "vets/index"})
-    public String listVets() {
+    public String listVets( Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
     }
     
 }
