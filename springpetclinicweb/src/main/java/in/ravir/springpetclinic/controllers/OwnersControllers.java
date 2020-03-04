e6e92cbe6bcbabadd942ca4f2dfb975e3425806d
Ravi Ranjan <ravi2519@gmail.com>
2020-03-04 05:46:19 -0800

    Added changes to list the owners closes #14

--- springpetclinicweb/src/main/java/in/ravir/springpetclinic/controllers/OwnersControllers.java
+++ springpetclinicweb/src/main/java/in/ravir/springpetclinic/controllers/OwnersControllers.java
@@ -1,7 +1,10 @@
 package in.ravir.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import in.ravir.springpetclinic.service.OwnerService;
 
 /**
  * OwnersControllers
@@ -9,9 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnersControllers {
 
+    private final OwnerService ownerService;
+
+    public OwnersControllers(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+    
+
     @RequestMapping({"/owners", "owners/index", "owners/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
+
     
 }
